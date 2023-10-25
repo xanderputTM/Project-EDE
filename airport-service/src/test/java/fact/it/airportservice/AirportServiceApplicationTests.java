a309153bc89f1b536089d51c90f9d0ad81902bae
Xander Put <putxander@hotmail.com>
2023-10-25 09:33:40 +0200

    Creation services

---
+++ airport-service/src/test/java/fact/it/airportservice/AirportServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fact.it.airportservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirportServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
