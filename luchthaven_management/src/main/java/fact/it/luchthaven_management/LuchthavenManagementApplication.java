d66f65e60d4f65e858dba050a368e9865a0799e0
Xander Put <putxander@hotmail.com>
2023-10-25 09:01:25 +0200

    Created project

---
+++ luchthaven_management/src/main/java/fact/it/luchthaven_management/LuchthavenManagementApplication.java
@@ -0,0 +1,13 @@
+package fact.it.luchthaven_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuchthavenManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LuchthavenManagementApplication.class, args);
+    }
+
+}
---
+++ luchthaven_management/src/test/java/fact/it/luchthaven_management/LuchthavenManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package fact.it.luchthaven_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuchthavenManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
