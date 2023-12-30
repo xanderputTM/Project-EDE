f30d37d3d00567a8a02aa745f20f3b3e4d2a0b58
Xander Put <r0721946@student.thomasmore.be>
2023-12-30 17:16:24 +0100

    added controller checks & a bit more dummy data

--- README.md
+++
@@ -1,5 +0,0 @@
-# Todo
-- Create person? Unique?
-- Delete passengers when deleting flight
-- Current airport code in docker compose?
-- 
--- gate-service/src/main/java/fact/it/gateservice/service/GateService.java
+++ gate-service/src/main/java/fact/it/gateservice/service/GateService.java
@@ -5,6 +5,8 @@ import fact.it.gateservice.model.Gate;
 import fact.it.gateservice.repository.GateRepository;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -20,16 +22,12 @@ public class GateService {
     @PostConstruct
     public void loadData() {
         if(gateRepository.count() <= 0){
-            Gate gate1 = new Gate();
-            gate1.setNumber("2");
-            gate1.setAirportCode("G0331");
-
-            Gate gate2 = new Gate();
-            gate2.setNumber("8");
-            gate2.setAirportCode("M032");
-
-            gateRepository.save(gate1);
-            gateRepository.save(gate2);
+            for (int i = 1; i <= 5; i++) {
+                Gate gate = new Gate();
+                gate.setNumber(String.valueOf(i));
+                gate.setAirportCode("M032");
+                gateRepository.save(gate);
+            }
         }
     }
 
@@ -46,9 +44,13 @@ public class GateService {
         return gates.stream().map(this::mapToGateResponse).toList();
     }
 
-    public GateDto getGateByAirportCodeAndGateNumber(String airportCode, String gateNumber) {
+    public ResponseEntity<Object> getGateByAirportCodeAndGateNumber(String airportCode, String gateNumber) {
+        if (!gateRepository.existsByAirportCodeAndNumber(airportCode, gateNumber)) {
+            return new ResponseEntity<>("There is no gate with that airport code and gate number!", HttpStatus.BAD_REQUEST);
+        }
+
         Gate gate = gateRepository.findByAirportCodeAndNumber(airportCode, gateNumber);
-        return mapToGateResponse(gate);
+        return new ResponseEntity<>(mapToGateResponse(gate), HttpStatus.OK);
     }
 
     private GateDto mapToGateResponse(Gate gate) {
